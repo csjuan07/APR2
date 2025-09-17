637f38812a63a291392f7ab05ffd7bf074c1d593
Juan Carlos dos Santos <105285336+csjuan07@users.noreply.github.com>
2025-09-17 10:40:29 -0300

    Atualização com exercícios do dia 15/09.
    Adição da função "mais_poderoso" e "movimenta". Melhora nos textos finais para mais clareza a respeito do que ocorreu ao fim da função.

--- simulado1.c
+++ simulado1.c
@@ -72,11 +72,72 @@ int ataca(int linha, int coluna, tpersonagem personagens[30], char tabuleiro[10]
         strcpy(nome_personagem,tabuleiro[linha][coluna]);
         int indice = procurar_personagem(nome_personagem,personagens);
         if (indice > -1){
-        	printf("\nPersonagem: %s",nome_personagem);
-        	printf("\nValor: %d",personagens[indice].poder);
+        	printf("\nPersonagem atacado: %s",nome_personagem);
+        	printf("\nPoder: %d",personagens[indice].poder);
             }
     	strcpy(tabuleiro[linha][coluna],"");
     }
+}
+
+void mais_poderoso(tpersonagem personagens[30]){
+	int i,indice=0,mais=0;
+    for (i=0;i<30;i++){
+    	if(personagens[i].poder>mais){
+        	mais = personagens[i].poder;
+        	indice = i;
+        }
+    }
+    printf("\nPersonagem mais poderoso: %s\n",personagens[indice].nome);
+}
+
+int movimenta(int coluna, int linha, char direcao[], int qtd, char tabuleiro[10][10][15]){
+	char nome_personagem[15];
+	if (linha > 9 || coluna > 9 || strcmp(tabuleiro[linha][coluna],"")==0){
+    	printf("\nPosição inicial de personagem fora do tabuleiro ou personagem não encontrado\n");
+		return 0;
+	}
+	else {
+    	strcpy(nome_personagem,tabuleiro[linha][coluna]);
+    	if (strcmp(direcao,"leste")==0){
+        	if ((coluna-qtd)>=0 && strcmp(tabuleiro[linha][coluna-qtd],"")==0){
+				strcpy(tabuleiro[linha][coluna-qtd],nome_personagem);
+                strcpy(tabuleiro[linha][coluna],"");
+                printf("\nO personagem %s andou %d posições para a direcao %s\n",nome_personagem,qtd,direcao);
+				return 1;
+            }
+            else return 0;
+        }
+        if (strcmp(direcao,"oeste")==0){
+        	if ((coluna+qtd)<=9 && strcmp(tabuleiro[linha][coluna+qtd],"")==0){
+				strcpy(tabuleiro[linha][coluna+qtd],nome_personagem);
+                strcpy(tabuleiro[linha][coluna],"");
+                printf("\nO personagem %s andou %d posições para a direcao %s\n",nome_personagem,qtd,direcao);
+				return 1;
+            }
+            else return 0;
+        }
+        if (strcmp(direcao,"norte")==0){
+            if ((linha-qtd)>=0 && strcmp(tabuleiro[linha-qtd][coluna],"")==0){
+				strcpy(tabuleiro[linha-qtd][coluna],nome_personagem);
+                strcpy(tabuleiro[linha][coluna],"");
+                printf("\nO personagem %s andou %d posições para a direcao %s\n",nome_personagem,qtd,direcao);
+				return 1;
+            }
+            else return 0;
+        }
+        if (strcmp(direcao,"sul")==0){
+        	if ((linha+qtd)<=9 && strcmp(tabuleiro[linha+qtd][coluna],"")==0){
+				strcpy(tabuleiro[linha+qtd][coluna],nome_personagem);
+                strcpy(tabuleiro[linha][coluna],"");
+                printf("\nO personagem %s andou %d posições para a direcao %s\n",nome_personagem,qtd,direcao);
+				return 1;
+            }
+            else {
+            printf("\nPosição de deslocamento final fora do tabuleiro ou já ocupada!\n");
+            return 0;
+            }
+        }
+	}
 }
 
 int main()
@@ -100,6 +161,7 @@ int main()
 	
 	imprime_tabuleiro(tabuleiro);
 	char nome[15]="princesa";
+    char dir[15]="sul";
 	printf("\n");
 	printf("\n");
 	posiciona(1,1,nome,tabuleiro);
@@ -107,11 +169,15 @@ int main()
 	int teste = ocorrencias("feiticeiro",tabuleiro);
 	printf("\n%d",teste);
     int teste_poder = poder_total(personagens,tabuleiro);
-    printf("\n%d",teste_poder);
+    printf("\nPoder total do tabuleiro: %d",teste_poder);
     ataca(1,1,personagens,tabuleiro);
     teste_poder = poder_total(personagens,tabuleiro);
-    printf("\n%d",teste_poder);
-	
+    printf("\nPoder total do tabuleiro: %d",teste_poder);
+	mais_poderoso(personagens);
+    movimenta(1,1,dir,6,tabuleiro);
+    
+    imprime_tabuleiro(tabuleiro);
 	return 0;
 }
+
 
